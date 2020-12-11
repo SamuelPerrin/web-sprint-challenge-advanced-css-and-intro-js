d00d05e047ad0d3becb01cfcd67eb42aca624233
MalHibou <swperrin51@gmail.com>
2020-12-11 11:13:40 -0600

    solve JS tasks 1 through 7

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const artists = [
+/*  const */ artists = [
     {
       "id": 0,
       "name": "Amedeo Modigliani",
@@ -208,16 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log(artists[0].name)
 
 //(2) Bio of the third artist (2nd index) in the array 
-
+console.log(artists[2].bio)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists[8].name = 'Vincent Van Gogh'
+console.log(artists[8].name)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,10 +229,10 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
-}  
-
+function getArtistByIndex(array, index) {
+  return `the artist at index ${array[index].id} is ${array[index].name}`
+}
+// console.log(getArtistByIndex(artists, 0))
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -242,10 +243,17 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    let years = array[i].years;
+    if (years.slice(0,2) === '19' && years.slice(7,9) === '19') {
+      result.push(array[i].name)
+    }
+  }
+  return result
 }
-
+// console.log(get20s(artists))
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -257,10 +265,11 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array,index){
+  array.splice(index,1)
+  return array.length
 }
-   
+// console.log(removeArtist(artists,0))
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -278,10 +287,18 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
-  }
-
+function addArtist(array){
+  array.push({
+    id: 20,
+    name: 'Samuel Perrin',
+    years: '1992 - 2020',
+    genre: 'Web Design',
+    nationality: 'American',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec purus nisl, tempor eget accumsan ac, rutrum vel enim. Nam mattis justo urna, id cursus nulla interdum sed. Sed id metus vitae nunc eleifend blandit aliquam sit amet magna. In ac neque est. Quisque et libero vel nulla placerat ornare id eu augue.'
+  })
+  return array
+}
+// console.log(addArtist(artists))
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -291,10 +308,16 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].paintings > 100) {
+      result.push(array[i].name)
+    }
+  }
+  return result
 }
-
+console.log(lotsOfArt(artists))
 
 
 
@@ -349,11 +372,11 @@ function randomize(/* Code here */){
   return 'bar';
 }
 /*Don't touch the code after this line! */
-export default{
+/* export  default{
   foo,
   getArtistByIndex,
   get20s,
   removeArtist,
   addArtist,
   lotsOfArt
-}
+} */
